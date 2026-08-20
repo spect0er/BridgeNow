48bbaa64b3650f49acaa75869b68ea76e878fbb1
Kartikey Mishra <mkartikey670@gmail.com>
2026-08-20 14:58:21 +0530

    apply CORS_ORIGIN config instead of allowing all origins

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 getDatabase();
 
 // Standard Express Middlewares
-app.use(cors());
+app.use(cors({ origin: config.corsOrigin }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
